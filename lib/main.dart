aee961fd26d63e43cc80defd41845ca0db3509e5
AI1411 <goethe0719@gmail.com>
2023-05-14 16:12:41 +0900

    リスト表示とページ遷移

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,38 @@
 import 'package:flutter/material.dart';
+import './poke_list_item.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Star',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const TopPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class TopPage extends StatelessWidget {
+  const TopPage({Key? key}) : super(key: key);
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Stack(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(32),
-                child: Image.network(
-                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-                    height: 100,
-                    width: 100),
-              ),
-              Container(
-                padding: const EdgeInsets.all(0),
-                child: const Text(
-                  'No.25',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const Text(
-            'Pikachu',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const Chip(label: Text('electric'), backgroundColor: Colors.yellow),
-        ],
-      )),
+      body: SafeArea(
+        child: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 16),
+          itemCount: 898,
+          itemBuilder: (context, index) => PokeListItem(index: index),
+        ),
+      ),
     );
   }
 }
---
+++ lib/poke_detail.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class PokeDetail extends StatelessWidget {
+  const PokeDetail({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Stack(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(32),
+                child: Image.network(
+                    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+                    height: 100,
+                    width: 100),
+              ),
+              Container(
+                padding: const EdgeInsets.all(0),
+                child: const Text(
+                  'No.25',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const Text(
+            'Pikachu',
+            style: TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Chip(label: Text('electric'), backgroundColor: Colors.yellow),
+        ],
+      )),
+    );
+  }
+}
